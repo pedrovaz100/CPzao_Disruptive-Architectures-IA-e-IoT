e9098342705cab60bf6313654610dc04fd8c3164
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-21 10:07:09 -0300

    feat: implementa alimentador automático IoT com ESP32

---
+++ .pio/build/esp32dev/lib58f/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/lib58f/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/labsfiap/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/labsfiap/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/labsfiap/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
